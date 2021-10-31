63fa6edf22ec97ce1e6f7d142c60b77a1933b66b
prog.shun0830@gmail.com <prog.shun0830@gmail.com>
2021-11-01 06:06:15 +0900

    update

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "Hello World";
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("0.0.0.0:9000")?
+        .run()
+        .await
 }
